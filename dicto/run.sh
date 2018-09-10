114e32e65d26faffc9776b2f16df0a84683b1838
Richard Klees <richard.klees@concepts-and-training.de>
2018-09-10 16:42:04 +0200

    more dicto-runner

--- dicto/run.sh
+++ dicto/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd ../..
-curl https://github.com/lechimp-p/dicto.php/releases/download/0.6.1/dicto.phar -o dicto.phar
-dicto.phar analyze CI-Results/dicto/config.yaml
+curl https://github.com/lechimp-p/dicto.php/releases/download/0.6.1/dicto.phar -L -o dicto.phar
+php -d memory_limit=-1 dicto.phar analyze CI-Results/dicto/config.yaml
+php -d memory_limit=-1 dicto.phar report diff CI-Results/dicto/dicto.yml | head -n -2 > CI-Results/dicto/reports/$(date).html
+cd CI-Results
+git add dicto/reports/*
+git add dicto/results/*
+git commit -a -m "new dicto-results"
+git push origin master
+cd ..
