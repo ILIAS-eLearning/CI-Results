eacc73fd0c55e13e3e1ff28d9bdec099483fd219
Guido Vollbach <gvollbach@databay.de>
2019-06-04 13:38:29 +0200

    - 2019-06-04-13:38:28 -

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DICTO_PATH="/tmp/dicto_latest.csv"
 PHPUNIT_PATH="/tmp/phpunit_latest.csv"
+PHPFIX_RESULTS_PATH="/tmp/phpfix_results"
 
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
 FOLDER_DATE=`date '+%Y-%m'`
@@ -20,7 +21,16 @@ then
 	cp "$DICTO_PATH" "results/$FOLDER_DATE/dicto_$TRAVIS_BUILD_NUMBER_$DATE.txt"
 fi
 
+if [ -e "$PHPFIX_RESULTS_PATH" ]
+then
+	cp "$PHPFIX_RESULTS_PATH" "results/$FOLDER_DATE/phpfix_$TRAVIS_BUILD_NUMBER_$DATE.json"
+fi
+
 git remote add results https://${ILIAS_VAR}@github.com/ILIAS-eLearning/CI-Results > /dev/null 2>&1
 git fetch
 git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE - $TRAVIS_EVENT_TYPE"
 git push --quiet --set-upstream results master
+if [ "$?" -ne 0 ]
+then
+	git pull && git push --quiet --set-upstream results master
+fi
